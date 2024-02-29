b31a16867dfdb7f66f90f92407afb878d5a51c3d
ryanmz1 <ryanmz1@outlook.com>
2024-02-29 15:13:25 +0800

    feat: Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   const [enabled, setEnabled] = useState(false)
 
   return (
     <>
-      <div data-theme="light">
-
+      <div className='grid grid-cols-1 gap-1 lg:grid-cols-2'>
+{/* 
         <div className='text-3xl font-bold underline'>Hello world!</div>
-        <button className="btn w-64 rounded-full">Button</button>
-        
+        <button className="btn w-64 rounded-full">Button</button> */}
+        <div className='lg:col-span-2 bg-red-500'>nav</div>
+        {/* <div>nav</div> */}
+        <div className='hidden lg:block bg-yellow-500'>aside</div>
+        <div className='bg-green-500'>main</div>
       </div>
     </>
   )
